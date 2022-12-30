88038054252d1214940b06b8afde7d726f113ec8
lucas-mior <lucas.mior@tutamail.com>
2022-12-30 17:47:54 -0300

    addweighted

--- src/find_pieces.py
+++ src/find_pieces.py
@@ -19,8 +19,8 @@ def draw_boxes(img):
         cv2.rectangle(canvas, (x0, y0), (x1, y1), color=color, thickness=thick)
         cv2.putText(canvas, f"{symbol} {conf}", (x0-5, y0-7),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, color, thick)
-        cv2.addWeighted(i, 0.6, canvas, 0.8, 0, canvas)
 
+    cv2.addWeighted(i, 0.6, canvas, 0.4, 0, canvas)
     img.yolopieces = canvas
     return img
 

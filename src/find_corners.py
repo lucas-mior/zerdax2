d1cc56ed49d20d47e41ffce459c653c8a6c898c9
lucas-mior <lucas.mior@tutamail.com>
2023-01-09 15:18:07 -0300

    save all

--- src/find_corners.py
+++ src/find_corners.py
@@ -12,13 +12,13 @@ DX = 50
 
 
 def find_corners(img):
-    img = black_space(img)
     img = create_cannys(img)
+    img = black_space(img)
     img = magic_prepare(img)
     lines = magic_lines(img)
     inter = aux.calc_intersections(img.gray3ch, lines)
     canvas = draw.intersections(img.gray3ch, inter)
-    # aux.save(img, "intersections", canvas)
+    aux.save(img, "intersections", canvas)
 
     img.corners = calc_corners(img, inter)
     img = perspective_transform(img)
@@ -30,8 +30,8 @@ def create_cannys(img):
     print("finding edges for gray, S, V images...")
     cannyG = aux.find_edges(img, img.G, lowpass=lf.ffilter)
     cannyV = aux.find_edges(img, img.V, lowpass=lf.ffilter)
-    # aux.save(img, "cannyG", cannyG)
-    # aux.save(img, "cannyV", cannyV)
+    aux.save(img, "cannyG", cannyG)
+    aux.save(img, "cannyV", cannyV)
 
     img.canny = cv2.bitwise_or(cannyG, cannyV)
     return img
@@ -41,9 +41,9 @@ def magic_prepare(img):
     print("preparing image for magic...")
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
     img.canny = cv2.morphologyEx(img.canny, cv2.MORPH_DILATE, kernel)
-    # aux.save(img, "canny_dilate", img.canny)
+    aux.save(img, "canny_dilate", img.canny)
     img.canny = cv2.morphologyEx(img.canny, cv2.MORPH_CLOSE, kernel)
-    # aux.save(img, "canny_closed", img.canny)
+    aux.save(img, "canny_closed", img.canny)
     return img
 
 
@@ -241,7 +241,7 @@ def calc_corners(img, inter):
     BR, BL, TR, TL = broad_corners(img, BR, BL, TR, TL)
 
     canvas = draw.corners(img.gray3ch, BR, BL, TR, TL)
-    # aux.save(img, "corners", canvas)
+    aux.save(img, "corners", canvas)
 
     return np.array([BR, BL, TR, TL], dtype='int32')
 
@@ -268,8 +268,8 @@ def perspective_transform(img):
     print("warping image...")
     img.wg = cv2.warpPerspective(img.G, img.warpMatrix, (width, height))
     img.wv = cv2.warpPerspective(img.V, img.warpMatrix, (width, height))
-    # aux.save(img, "warpclaheG", img.wg)
-    # aux.save(img, "warpclaheV", img.wv)
+    aux.save(img, "warpclaheG", img.wg)
+    aux.save(img, "warpclaheV", img.wv)
 
     return img
 
@@ -345,6 +345,7 @@ def black_space(img):
     img.G = make_border(img.G)
     img.V = make_border(img.V)
     img.gray3ch = make_border(img.gray3ch)
+    img.canny = make_border(img.canny)
 
     img.bwidth += (DX*2)
     img.bheigth += (DX*2)

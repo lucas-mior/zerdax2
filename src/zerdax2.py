d6999fd50555d02c13a24154a6c4d01e0b303043
lucas-mior <lucas.mior@tutamail.com>
2023-01-17 17:32:50 -0300

    parse filenames and loglevel using argparse

--- src/zerdax2.py
+++ src/zerdax2.py
@@ -2,14 +2,35 @@
 
 import sys
 import logging as log
+import argparse
 from algorithm import algorithm
 
-if __name__ == '__main__':
-    level = log.ERROR
-    fmt = '[%(levelname)s] %(message)s'
-    log.basicConfig(level=level, format=fmt)
 
-    for filename in sys.argv[1:]:
+def parse_args(args):
+    loglevels = {
+        'critical': log.CRITICAL,
+        'error': log.ERROR,
+        'warning': log.WARNING,
+        'info': log.INFO,
+        'debug': log.DEBUG
+    }
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*')
+    parser.add_argument('--loglevel', dest='level',
+                        default='error', choices=loglevels.keys(),
+                        help='verbosity level')
+    args = parser.parse_args(args)
+
+    level = loglevels[args.level]
+    return args.filenames, level
+
+
+if __name__ == '__main__':
+    filenames, loglevel = parse_args(sys.argv[1:])
+    fmt = '[%(levelname)s] %(message)s'
+    log.basicConfig(level=loglevel, format=fmt)
+
+    for filename in filenames:
         print(f"============ zerdax2.py {filename} ============")
         fen = algorithm(filename)
         print(f"FEN({filename}): {fen}")

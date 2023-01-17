f3ff9fe3688c306a4cb6ef7075e26081cc5a7327
lucas-mior <lucas.mior@tutamail.com>
2023-01-17 14:51:49 -0300

    setup logging

--- src/zerdax2.py
+++ src/zerdax2.py
@@ -2,8 +2,13 @@
 
 import sys
 from algorithm import algorithm
+import logging as log
 
 if __name__ == '__main__':
+    level = log.INFO
+    fmt = '[%(levelname)s] %(message)s'
+    log.basicConfig(level=level, format=fmt)
+
     for filename in sys.argv[1:]:
         print(f"============ zerdax2.py {filename} ============")
         fen = algorithm(filename)

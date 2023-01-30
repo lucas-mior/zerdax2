326c14ff462d6170022b1b30aba97d0c64c14ce8
lucas-mior <lucas.mior@tutamail.com>
2023-01-30 14:57:51 -0300

    organize segments.c

--- c_segments.c
+++ c_segments.c
@@ -36,8 +36,8 @@ bool segs_intersect(int32_t * restrict line0, int32_t * restrict line1) {
     if (delta == 0)
         return false;
 
-    double s = ((double)dx0*((double)yy0 - (double)y0) + (double)dy0*((double)x0 - (double)xx0)) / (double)delta;
-    double t = ((double)dx1*((double)y0 - (double)yy0) + (double)dy1*((double)xx0 - (double)x0)) / (double)(-delta);
+    double s = (double) (dx0*yy0 - y0 + dy0*x0 - xx0) / (double)delta;
+    double t = (double) (dx1*y0 - yy0 + dy1*xx0 - x0) / (double)(-delta);
 
     bool ss = (0 <= s) && (s <= 1);
     bool tt = (0 <= t) && (t <= 1);
@@ -55,23 +55,22 @@ int32_t point_seg_dist(int32_t const px, int32_t const py, int32_t * restrict li
     if ((dx == dy) && (dy == 0)) {
         dx = px - x0;
         dy = py - y0;
-        goto calc_dist;
-    }
-    double t = (((double)px - (double)x0)*(double)dx + ((double)py - (double)y0)*(double)dy) / ((double)dx*(double)dx + (double)dy*(double)dy);
-
-    if (t < 0) {
-        dx = px - x0;
-        dy = py - y0;
-    } else if (t > 1) {
-        dx = px - x1;
-        dy = py - y1;
     } else {
-        double near_x = x0 + t*dx;
-        double near_y = y0 + t*dy;
-        dx = px - near_x;
-        dy = py - near_y;
+        double t = (double) ((px - x0)*dx + (py - y0)*dy) / (double) (dx*dx + dy*dy);
+
+        if (t < 0) {
+            dx = px - x0;
+            dy = py - y0;
+        } else if (t > 1) {
+            dx = px - x1;
+            dy = py - y1;
+        } else {
+            double near_x = x0 + t*dx;
+            double near_y = y0 + t*dy;
+            dx = px - near_x;
+            dy = py - near_y;
+        }
     }
-    calc_dist:
     return sqrt(dx*dx + dy*dy);
 }
 

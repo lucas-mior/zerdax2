a5ad2e0d4aee76363fe834f64ace39b0aaab3421
lucas-mior <lucas.mior@tutamail.com>
2023-05-20 16:57:49 -0300

    add angles.py

--- lines.py
+++ lines.py
@@ -1,11 +1,11 @@
 import numpy as np
 import cv2
 import logging as log
-from jenkspy import jenks_breaks
 
 import algorithm as algo
 import intersect as intersect
 import constants as consts
+import angles as angles
 import drawings as draw
 from c_load import segments_distance
 from c_load import lines_bundle
@@ -53,12 +53,12 @@ def find_warped_lines(canny):
             draw.save("hough_lines", canvas)
 
         lines_hough, _ = length_theta(lines_hough, abs_angle=False)
-        lines_hough = filter_90(lines_hough)
+        lines_hough = angles.filter_not_right(lines_hough)
         if algo.debug:
             canvas = draw.lines(canny_3channels, lines_hough)
             draw.save("filter90", canvas)
 
-        vert, hori = split(lines_hough)
+        vert, hori = angles.split(lines_hough)
         lv, lh = check_save("split", vert, hori, 0, 0)
         vert, hori = sort(vert, hori)
         lv, lh = check_save("sort", vert, hori, lv, lh)
@@ -155,10 +155,10 @@ def find_baselines(canny):
             draw.save("hough_lines", canvas)
 
         lines_hough, _ = length_theta(lines_hough, abs_angle=False)
-        vert, hori = split(lines_hough)
+        vert, hori = angles.split(lines_hough)
         lv, lh = check_save("split", vert, hori, 0, 0)
-        vert, hori = filter_byangle(vert, hori)
-        lv, lh = check_save("filter_byangle", vert, hori, lv, lh)
+        vert, hori = angles.filter_misdirected(vert, hori)
+        lv, lh = check_save("filter_misdirected", vert, hori, lv, lh)
         vert, hori = sort(vert, hori)
         lv, lh = check_save("sort", vert, hori, lv, lh)
         if vert is None or hori is None:
@@ -167,8 +167,8 @@ def find_baselines(canny):
         vert = bundle_lines(vert, distv)
         hori = bundle_lines(hori, disth)
         lv, lh = check_save("lines_bundled", vert, hori, lv, lh)
-        vert, hori = filter_byinter(vert, hori)
-        lv, lh = check_save("filter_byinter", vert, hori, lv, lh)
+        vert, hori = angles.filter_intersecting(vert, hori)
+        lv, lh = check_save("filter_intersecting", vert, hori, lv, lh)
 
         ll = lv + lh
         log.info(f"{ll} # {lv},{lh} @ {angle}º, {tvotes=},{minlen=},{maxgap=}")
@@ -186,123 +186,6 @@ def find_baselines(canny):
     vert, hori = sort(vert, hori)
     lv, lh = check_save("sort", vert, hori, 0, 0)
     return vert, hori
-
-
-def split(lines):
-    log.info("spliting lines into vertical and horizontal...")
-
-    def _check_split(vert, hori):
-        if abs(np.median(hori[:, 5]) - np.median(vert[:, 5])) < 40*100:
-            return False
-        else:
-            return True
-
-    if (lines.shape[1] < 6):
-        lines, _ = length_theta(lines)
-    angles = lines[:, 5]
-
-    try:
-        limits = jenks_breaks(angles, n_classes=3)
-    except Exception:
-        return None, None
-
-    a0 = angles[angles <= limits[1]]
-    a1 = angles[(limits[1] < angles) & (angles <= limits[2])]
-    a2 = angles[limits[2] < angles]
-    centers = [np.median(a0), np.median(a1), np.median(a2)]
-
-    d0 = abs(centers[0] - centers[1])
-    d1 = abs(centers[0] - centers[2])
-    d2 = abs(centers[1] - centers[2])
-
-    maxdiff = consts.angles_max_diff
-    dd0 = d0 < maxdiff and d1 > maxdiff and d2 > maxdiff
-    dd1 = d1 < maxdiff and d0 > maxdiff and d2 > maxdiff
-    dd2 = d2 < maxdiff and d0 > maxdiff and d1 > maxdiff
-
-    if dd0 or dd1 or dd2:
-        try:
-            limits = jenks_breaks(angles, n_classes=2)
-        except Exception:
-            return None, None
-        hori = lines[angles <= limits[1]]
-        vert = lines[limits[1] < angles]
-        if not _check_split(vert, hori):
-            return None, None
-    else:
-        for line in lines:
-            if line[5] < (-45 * 100):
-                line[5] = -line[5]
-        angles = lines[:, 5]
-        try:
-            limits = jenks_breaks(angles, n_classes=2)
-        except Exception:
-            return None, None
-        hori = lines[angles <= limits[1]]
-        vert = lines[limits[1] < angles]
-        if not _check_split(vert, hori):
-            return None, None
-
-    if abs(np.median(vert[:, 5])) < abs(np.median(hori[:, 5])):
-        aux = vert
-        vert = hori
-        hori = aux
-
-    for line in vert:
-        if line[1] > line[3]:
-            a1, b1 = line[0], line[1]
-            line[0], line[1] = line[2], line[3]
-            line[2], line[3] = a1, b1
-    return vert, hori
-
-
-def filter_byangle(vert, hori=None):
-    log.info("filtering lines by angle accoring to direction...")
-    tolerance = consts.angle_tolerance
-
-    def _filter(lines):
-        angle = np.median(lines[:, 5])
-        right = np.abs(lines[:, 5] - angle) <= tolerance
-        return lines[right]
-
-    if vert is not None:
-        vert = _filter(vert)
-    if hori is not None:
-        hori = _filter(hori)
-    return vert, hori
-
-
-def filter_byinter(vert, hori=None):
-    log.info("filtering lines by number of intersections ...")
-    limit = True
-
-    def _filter(lines):
-        for i, line in enumerate(lines):
-            inters = intersect.calculate_all(np.array([line]), lines, limit)
-            if inters is None or len(inters) == 0:
-                continue
-            elif inters.shape[1] >= 4:
-                lines = np.delete(lines, i, axis=0)
-                break
-        return lines
-
-    for j in range(3):
-        if vert is not None:
-            vert = _filter(vert)
-        if hori is not None:
-            hori = _filter(hori)
-    return vert, hori
-
-
-def filter_90(lines):
-    remove = np.zeros(lines.shape[0], dtype='uint8')
-
-    for i, t in enumerate(lines[:, 5]):
-        if abs(t - 90*100) > 4*100 and abs(t + 90*100) > 4*100:
-            if abs(t) > 4*100:
-                remove[i] = 1
-
-    return lines[remove == 0]
 
 
 def sort(vert, hori=None, kind=0):

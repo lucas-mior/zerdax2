d6b4f6a750375ab5aafda49392c26079607d0b6d
lucas-mior <lucas.mior@tutamail.com>
2023-05-29 21:00:55 -0300

    style

--- misc.py
+++ misc.py
@@ -1,98 +1,49 @@
 COLORS = {
     0: (120, 0, 210),
-    1: (0, 0, 255),
-    2: (0, 140, 140),
-    3: (0, 255, 0),
-    4: (140, 140, 0),
-    5: (255, 0, 0),
-    6: (140, 0, 140),
-    7: (0, 0, 255),
-    8: (0, 100, 100),
-    9: (0, 255, 0),
-    10: (100, 100, 0),
-    11: (255, 0, 0),
-    12: (100, 0, 100)
+    1: (0, 0, 255), 2: (0, 140, 140), 3: (0, 255, 0),
+    4: (140, 140, 0), 5: (255, 0, 0), 6: (140, 0, 140),
+    7: (0, 0, 255), 8: (0, 100, 100), 9: (0, 255, 0),
+    10: (100, 100, 0), 11: (255, 0, 0), 12: (100, 0, 100)
 }
 
 SYMBOLS = {
-    -1: "1",
-    0: "Board",
-    1: "K",
-    2: "Q",
-    3: "R",
-    4: "B",
-    5: "N",
-    6: "P",
-    7: "k",
-    8: "q",
-    9: "r",
-    10: "b",
-    11: "n",
-    12: "p"
+    -1: "1", 0: "Board",
+    1: "K", 2: "Q", 3: "R",
+    4: "B", 5: "N", 6: "P",
+    7: "k", 8: "q", 9: "r",
+    10: "b", 11: "n", 12: "p"
 }
 
 NAMES = {
     0: "Board",
-    1: "WhiteKing",
-    2: "WhiteQueen",
-    3: "WhiteRook",
-    4: "WhiteBishop",
-    5: "WhiteKnight",
-    6: "WhitePawn",
-    7: "BlackKing",
-    8: "BlackQueen",
-    9: "BlackRook",
-    10: "BlackBishop",
-    11: "BlackKnight",
-    12: "BlackPawn"
+    1: "WhiteKing", 2: "WhiteQueen", 3: "WhiteRook",
+    4: "WhiteBishop", 5: "WhiteKnight", 6: "WhitePawn",
+    7: "BlackKing", 8: "BlackQueen", 9: "BlackRook",
+    10: "BlackBishop", 11: "BlackKnight", 12: "BlackPawn"
 }
 
 PIECES = {
     "Board": "Board",
-    "K": "WhiteKing",
-    "Q": "WhiteQueen",
-    "R": "WhiteRook",
-    "B": "WhiteBishop",
-    "N": "WhiteKnight",
-    "P": "WhitePawn",
-    "k": "BlackKing",
-    "q": "BlackQueen",
-    "r": "BlackRook",
-    "b": "BlackBishop",
-    "n": "BlackKnight",
-    "p": "BlackPawn",
+    "K": "WhiteKing", "Q": "WhiteQueen", "R": "WhiteRook",
+    "B": "WhiteBishop", "N": "WhiteKnight", "P": "WhitePawn",
+    "k": "BlackKing", "q": "BlackQueen", "r": "BlackRook",
+    "b": "BlackBishop", "n": "BlackKnight", "p": "BlackPawn",
 }
 
 CLASSES = {
     "Board": 0,
-    "K": 1,
-    "Q": 2,
-    "R": 3,
-    "B": 4,
-    "N": 5,
-    "P": 6,
-    "k": 1,
-    "q": 2,
-    "r": 3,
-    "b": 4,
-    "n": 5,
-    "p": 6
+    "K": 1, "Q": 2, "R": 3,
+    "B": 4, "N": 5, "P": 6,
+    "k": 1, "q": 2, "r": 3,
+    "b": 4, "n": 5, "p": 6
 }
 
 NUMBERS = {
     "Board": 0,
-    "K": 1,
-    "Q": 2,
-    "R": 3,
-    "B": 4,
-    "N": 5,
-    "P": 6,
-    "k": 7,
-    "q": 8,
-    "r": 9,
-    "b": 10,
-    "n": 11,
-    "p": 12
+    "K": 1, "Q": 2, "R": 3,
+    "B": 4, "N": 5, "P": 6,
+    "k": 7, "q": 8, "r": 9,
+    "b": 10, "n": 11, "p": 12
 }
 
 AMOUNT = {

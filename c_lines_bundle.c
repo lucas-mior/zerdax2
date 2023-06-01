5c7171194ccb3cada0174892e17d8211de751b17
lucas-mior <lucas.mior@tutamail.com>
2023-05-31 22:02:22 -0300

    organize bundle_lines.c

--- c_lines_bundle.c
+++ c_lines_bundle.c
@@ -27,65 +27,11 @@ typedef struct Group {
 static Group *first = NULL;
 static Group *last = NULL;
 
-static int32 compare(const void *a, const void *b) {
-    int32 *c = (int32 *) a;
-    int32 *d = (int32 *) b;
-    return *c - *d;
-}
-
-static int32 median(int32 *array, int32 length) {
-    qsort(array, length, sizeof(int32), compare);
-    if ((length % 2) == 0) {
-        return (array[(int)((length/2) - 1)] + array[(int) (length/2)]) / 2.0;
-    } else {
-        return array[(int)(length/2)];
-    }
-}
-
-static void append(Group *group, int32 line[LINE_FIELDS]) {
-    int32 j = group->length;
-    if (j >= MAX_LINES_IN_GROUP)
-        return;
-
-    group->angles[j] = line[I_ANGLE];
-    memcpy(group->lines[j], line, line_size);
-    group->length += 1;
-    return;
-}
-
-static void groups_append(int32 line[LINE_FIELDS]) {
-    Group *group = last;
-    group->next = util_alloc(NULL, sizeof(Group));
-    group = group->next;
-    group->next = NULL;
-    group->length = 0;
-    append(group, line);
-    last = group;
-    return;
-}
-
-static bool check_line_diff(int32 line1[LINE_FIELDS], Group *group) {
-    int32 min_angle2 = min_angle + 2;
-    while (group) {
-        for (int i = 0; i < group->length; i += 1) {
-            int32 *line0 = group->lines[i];
-            int32 dtheta = abs(line1[I_ANGLE] - line0[I_ANGLE]);
-            if (dtheta < min_angle) {
-                if (segments_distance(line0, line1) < min_distance) {
-                    append(group, line1);
-                    return false;
-                }
-            } else if (dtheta <= min_angle2 || abs(dtheta - 180*100) < min_angle) {
-                if (segments_distance(line0, line1) <= 1) {
-                    append(group, line1);
-                    return false;
-                }
-            }
-        }
-        group = group->next;
-    }
-    return true;
-}
+static int32 compare(const void *, const void *);
+static int32 median(int32 *, int32);
+static void append(Group *, int32 [LINE_FIELDS]);
+static void groups_append(int32 [LINE_FIELDS]);
+static bool check_line_diff(int32 [LINE_FIELDS], Group *);
 
 int32 lines_bundle(int32 lines[][LINE_FIELDS], int32 bundled[][LINE_FIELDS], int32 n, int32 min_distance0) {
     min_distance = min_distance0;
@@ -134,4 +80,64 @@ int32 lines_bundle(int32 lines[][LINE_FIELDS], int32 bundled[][LINE_FIELDS], int
         m += 1;
     }
     return m;
+}
+
+int32 compare(const void *a, const void *b) {
+    int32 *c = (int32 *) a;
+    int32 *d = (int32 *) b;
+    return *c - *d;
+}
+
+int32 median(int32 *array, int32 length) {
+    qsort(array, length, sizeof(int32), compare);
+    if ((length % 2) == 0) {
+        return (array[(int)((length/2) - 1)] + array[(int) (length/2)]) / 2.0;
+    } else {
+        return array[(int)(length/2)];
+    }
+}
+
+void append(Group *group, int32 line[LINE_FIELDS]) {
+    int32 j = group->length;
+    if (j >= MAX_LINES_IN_GROUP)
+        return;
+
+    group->angles[j] = line[I_ANGLE];
+    memcpy(group->lines[j], line, line_size);
+    group->length += 1;
+    return;
+}
+
+void groups_append(int32 line[LINE_FIELDS]) {
+    Group *group = last;
+    group->next = util_alloc(NULL, sizeof(Group));
+    group = group->next;
+    group->next = NULL;
+    group->length = 0;
+    append(group, line);
+    last = group;
+    return;
+}
+
+bool check_line_diff(int32 line1[LINE_FIELDS], Group *group) {
+    int32 min_angle2 = min_angle + 2;
+    while (group) {
+        for (int i = 0; i < group->length; i += 1) {
+            int32 *line0 = group->lines[i];
+            int32 dtheta = abs(line1[I_ANGLE] - line0[I_ANGLE]);
+            if (dtheta < min_angle) {
+                if (segments_distance(line0, line1) < min_distance) {
+                    append(group, line1);
+                    return false;
+                }
+            } else if (dtheta <= min_angle2 || abs(dtheta - 180*100) < min_angle) {
+                if (segments_distance(line0, line1) <= 1) {
+                    append(group, line1);
+                    return false;
+                }
+            }
+        }
+        group = group->next;
+    }
+    return true;
 }

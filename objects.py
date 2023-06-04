d363c0fd4295955ec4bcb365d1d7e359df63aa1c
lucas-mior <lucas.mior@tutamail.com>
2023-06-04 17:40:37 -0300

    use kmeans

--- objects.py
+++ objects.py
@@ -49,35 +49,44 @@ def detect(BGR):
 
 
 def determine_colors(pieces, image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    hue = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)[:, :, 0]
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    avg_colors = np.empty((len(pieces), 2), dtype='float32')
+    avg_colors = np.empty((len(pieces), 3), dtype='float32')
     for i, p in enumerate(pieces):
         x0, y0, x1, y1 = p[:4]
         x0 += 10
         y0 += 10
         x1 -= 10
         y1 -= 10
-        gray_box = gray[y0:y1, x0:x1]
-        hue_box = hue[y0:y1, x0:x1]
-        avg_colors[i, 0] = np.median(gray_box, overwrite_input=True)
-        avg_colors[i, 1] = np.median(hue_box, overwrite_input=True)
+        box_h = hsv[y0:y1, x0:x1, 0]
+        box_s = hsv[y0:y1, x0:x1, 1]
+        box_v = hsv[y0:y1, x0:x1, 2]
+        avg_colors[i, 0] = np.median(box_h, overwrite_input=True)
+        avg_colors[i, 1] = np.median(box_s, overwrite_input=True)
+        avg_colors[i, 2] = np.median(box_v, overwrite_input=True)
 
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 
     ret, labels, centers = cv2.kmeans(avg_colors, 2, None,
                                       criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
     labels = np.ravel(labels)
-    print(labels)
+    print(centers)
     black = pieces[labels == 0]
     white = pieces[labels == 1]
+    if centers[1, 2] < centers[0, 2]:
+        aux = black
+        black = white
+        white = aux
+
     black[:, 5] += 6
     pieces = np.vstack((black, white))
     return pieces
 
 
 def remove_captured_pieces(pieces, boardbox):
+    if boardbox is None:
+        return pieces
+
     xmin = np.minimum(pieces[:, 2], boardbox[2])
     xmax = np.maximum(pieces[:, 0], boardbox[0])
     inter_x = np.maximum(0, xmin - xmax)

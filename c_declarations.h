e31cc3566b6920fd002ecb53308f70884b8aae66
lucas-mior <lucas.mior@tutamail.com>
2023-09-11 11:28:48 -0300

    style

--- c_declarations.h
+++ c_declarations.h
@@ -7,7 +7,7 @@
 
 typedef int32_t int32;
 
-int32 segments_distance(int32 * restrict, int32 * restrict);
+int32 segments_distance(int32 *restrict, int32 *restrict);
 void *util_malloc(size_t);
 void *util_realloc(void *, size_t);
 void *util_calloc(size_t, size_t);

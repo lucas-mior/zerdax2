955c5405d9529025581a1917d72a7deb3726167e
lucas-mior <lucas.mior@tutamail.com>
2024-05-03 20:44:00 -0300

    clean up

--- c_declarations.h
+++ c_declarations.h
@@ -2,11 +2,7 @@
 #define C_DECLARATIONS_H
 
 #include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <stdbool.h>
-#include <math.h>
 
 typedef int32_t int32;
 typedef uint8_t uint8;

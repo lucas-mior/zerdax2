bf353c0061a1736955811cdd084e07a46de8d59b
lucas-mior <lucas.mior@tutamail.com>
2026-07-27 03:52:04 -0300

    wip

--- c_declarations.h
+++ c_declarations.h
@@ -1,10 +1,7 @@
 #ifndef C_DECLARATIONS_H
 #define C_DECLARATIONS_H
 
-#include <stdint.h>
-#include <stdbool.h>
-
-#include "primitives.h"
+#include "cbase.h"
 
 int32 segments_distance(int32 *restrict line0, int32 *restrict line1);
 

1bf981a0afdb85204f5b13f01c9fd293469ca5a1
lucas-mior <lucas.mior@tutamail.com>
2026-08-07 14:38:38 -0300

    organize all build.sh files similarly

--- build.sh
+++ build.sh
@@ -67,11 +67,15 @@ if echo "$OS" | grep -q "Linux"; then
     fi
 fi
 
-if [ "$target" = "test" ] && [ -z "${CC:-}" ] && command -v tcc >/dev/null 2>&1; then
-    CC=tcc
-else
-    CC="${CC:-cc}"
-fi
+requested_cc=${CC:-}
+case "$target" in
+"debug"|"test"|"fast_feedback")
+    CC="${requested_cc:-tcc}"
+    ;;
+*)
+    CC="${requested_cc:-cc}"
+    ;;
+esac
 if [ "$CC" = "clang" ]; then
     CFLAGS="$CFLAGS -Weverything"
     CFLAGS="$CFLAGS -Wno-unsafe-buffer-usage"
@@ -97,7 +101,6 @@ case "$target" in
     CFLAGS="$CFLAGS $GNUSOURCE -g3 -O2 -fPIC -flto -march=native -ftree-vectorize"
     ;;
 "fast_feedback")
-    CC=clang
     CFLAGS="$CFLAGS $GNUSOURCE -fPIC -Werror"
     ;;
 "test"|"install"|"uninstall"|"clean")
